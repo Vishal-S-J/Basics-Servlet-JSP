5c87af45a351ff270bb1dff15b2bc98002f52f88
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-06 13:13:17 -0500

    full code updated

--- Chapter-2/src/main/java/com/servlet/Add.java
+++ Chapter-2/src/main/java/com/servlet/Add.java
@@ -1,6 +1,5 @@
 package com.servlet;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,7 +9,7 @@ import java.io.PrintWriter;
 
 public class Add extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
 
@@ -18,5 +17,8 @@ public class Add extends HttpServlet {
         //getWriter :Returns a PrintWriter object that can send character text to the client.
         PrintWriter printWriter = resp.getWriter();
         printWriter.println("SUM IS :: "+(num1+num2));
+
+        //Also, for checking purpose we are printing same data in console
+        System.out.println("SUM IS :: "+(num1+num2));
     }
 }
