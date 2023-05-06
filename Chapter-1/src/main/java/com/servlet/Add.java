a6fcc7efc17cc5644a2d28b713468b1f1c8adc76
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-06 11:31:39 -0500

    req.getParameter() explained

--- Chapter-1/src/main/java/com/servlet/Add.java
+++ Chapter-1/src/main/java/com/servlet/Add.java
@@ -7,9 +7,12 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Add extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        //to get data from the html page we should request parameters from the page
+        //like in this case we are getting the data of num1 to num2 from html page to java
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
 
+        //Since this statement is written in java it will print the data on console
         System.out.println("ADDITION IS :: "+(num1+num2));
     }
 }
