312000fa1b4a6afba43d72324b7cab2e02cc463d
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-06 23:36:37 -0500

    Welcome Page Servlet added

---
+++ Chapter-3/src/main/java/com/servlet/Welcome.java
@@ -0,0 +1,19 @@
+package com.servlet;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Welcome extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+
+        String username = req.getParameter("uname");
+        printWriter.println("WELCOME :: "+username);
+    }
+}
