55007398260a0e19c5128b86bb541bdcf039495f
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-07 19:59:29 -0500

    CookiesServlet.java updated

--- Chapter-4/src/main/java/com/servlet/CookiesServlet.java
+++ Chapter-4/src/main/java/com/servlet/CookiesServlet.java
@@ -1,17 +1,15 @@
 package com.servlet;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 
 public class CookiesServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         try {
             resp.setContentType("text/html");
             PrintWriter printWriter = resp.getWriter();
