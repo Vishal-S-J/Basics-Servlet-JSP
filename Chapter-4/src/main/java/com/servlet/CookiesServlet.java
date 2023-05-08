c4c7a841342e6bf2367a56a4531fb80a9c52ffb0
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-07 19:58:39 -0500

    CookiesServlet.java added

---
+++ Chapter-4/src/main/java/com/servlet/CookiesServlet.java
@@ -0,0 +1,35 @@
+package com.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CookiesServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            resp.setContentType("text/html");
+            PrintWriter printWriter = resp.getWriter();
+
+            String username = req.getParameter("username");
+            printWriter.println("WELCOME :: "+username.toUpperCase());
+
+            Cookie cookie = new Cookie("username", username);   //creating the cookies object
+            resp.addCookie(cookie);
+
+            //creating the form
+            printWriter.println("<form action='servlet2' method='post'>");
+            printWriter.println("<input type='submit' value='NEXT PAGE' />");
+            printWriter.println("</form>");
+
+            printWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
