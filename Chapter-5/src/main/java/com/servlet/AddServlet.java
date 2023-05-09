07268d73a52777c0dc53b051db09e446fb9fbe8f
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-08 20:28:04 -0500

    AddServlet.java updated

--- Chapter-5/src/main/java/com/servlet/AddServlet.java
+++ Chapter-5/src/main/java/com/servlet/AddServlet.java
@@ -1,6 +1,5 @@
 package com.servlet;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,7 +10,7 @@ import java.io.PrintWriter;
 
 public class AddServlet extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
 
         PrintWriter printWriter = resp.getWriter();
