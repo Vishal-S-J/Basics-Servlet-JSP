dd04b74aebe5ef20d13ebba933d5095d827c28b5
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-08 20:37:01 -0500

    DetailsServlet.java updated

--- Chapter-5/src/main/java/com/servlet/DetailsServlet.java
+++ Chapter-5/src/main/java/com/servlet/DetailsServlet.java
@@ -1,38 +1,38 @@
 package com.servlet;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 
 public class DetailsServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try{
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
             resp.setContentType("text/html");
             PrintWriter out = resp.getWriter();
 
             HttpSession session = req.getSession(false);
-            String fname=(String)session.getAttribute("fname");
-            String lname=(String)session.getAttribute("lname");
-            int age= (int) session.getAttribute("age");
-            String address=(String)session.getAttribute("address");
+            String fname = (String) session.getAttribute("fname");
+            String lname = (String) session.getAttribute("lname");
+            int age = (int) session.getAttribute("age");
+            String address = (String) session.getAttribute("address");
 
             out.println("HELLO " + fname.toUpperCase());
             out.println("\n========================================");
             out.println("\n               DETAILS                  ");
             out.println("\n========================================");
-            out.println("\nFIRST NAME :: "+fname);
-            out.println("\nLAST NAME :: "+lname);
-            out.println("\nAGE :: "+age);
-            out.println("\nADDRESS :: "+address);
+            out.println("\nFIRST NAME :: " + fname);
+            out.println("\nLAST NAME :: " + lname);
+            out.println("\nAGE :: " + age);
+            out.println("\nADDRESS :: " + address);
             out.println("\n========================================");
             out.close();
 
-        }catch(Exception e){System.out.println(e);}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
