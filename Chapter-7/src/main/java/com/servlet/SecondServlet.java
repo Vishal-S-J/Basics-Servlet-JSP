70309970c4d97e16f3c87192cfef93bff8cf8aa1
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-11 22:18:51 -0500

    SecondServlet updated

--- Chapter-7/src/main/java/com/servlet/SecondServlet.java
+++ Chapter-7/src/main/java/com/servlet/SecondServlet.java
@@ -1,4 +1,25 @@
 package com.servlet;
 
-public class SecondServlet {
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+
+public class SecondServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            resp.setContentType("text/html");
+            PrintWriter printWriter = resp.getWriter();
+
+            //Getting the value from the hidden field
+            String name = req.getParameter("uname");
+            printWriter.print("Hello " + name);
+
+            printWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
