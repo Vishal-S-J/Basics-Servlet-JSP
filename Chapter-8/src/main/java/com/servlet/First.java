11f9304c9bab3896deedc2a8e935095b04cbb36c
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-12 18:56:05 -0500

    servlet class First updated

--- Chapter-8/src/main/java/com/servlet/First.java
+++ Chapter-8/src/main/java/com/servlet/First.java
@@ -1,4 +1,24 @@
 package com.servlet;
 
-public class First {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+
+@WebServlet("/firstPage")
+public class First extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            resp.setContentType("text/html");
+            PrintWriter printWriter = resp.getWriter();
+
+            printWriter.println("<h3> HELLO WORLD </h3>");
+            printWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
