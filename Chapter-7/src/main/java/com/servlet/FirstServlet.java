70c3db7f981ff705db91a0ec9dfe447e620fb56d
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-05-11 22:17:13 -0500

    first servlet updated

--- Chapter-7/src/main/java/com/servlet/FirstServlet.java
+++ Chapter-7/src/main/java/com/servlet/FirstServlet.java
@@ -1,4 +1,30 @@
 package com.servlet;
 
-public class FirstServlet {
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            resp.setContentType("text/html");
+            PrintWriter printWriter = resp.getWriter();
+
+            String name = req.getParameter("name");
+            printWriter.print("Welcome " + name);
+
+            //creating form that have invisible textfield
+            printWriter.print("<form action='secondPage'>");
+            printWriter.print("<input type='hidden' name='uname' value='" + name + "'>");
+            printWriter.print("<input type='submit' value='go'>");
+            printWriter.print("</form>");
+            printWriter.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
