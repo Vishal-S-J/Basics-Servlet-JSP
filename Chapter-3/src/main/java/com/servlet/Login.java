bc861c6d19a07eee88a96da4e4b29ceee3012c9d
Vishal Jadhav <129414799+Vishal-S-J@users.noreply.github.com>
2023-05-20 10:02:02 -0500

    Update Login.java

--- Chapter-3/src/main/java/com/servlet/Login.java
+++ Chapter-3/src/main/java/com/servlet/Login.java
@@ -12,6 +12,7 @@ import java.io.PrintWriter;
 public class Login extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // this statement gets the data from the html page    //where uname and pass are names of the tags in html page
         String username = req.getParameter("uname");
         String password = req.getParameter("pass");
 
@@ -23,6 +24,8 @@ public class Login extends HttpServlet {
             requestDispatcher.forward(req, resp);
         } else {
             printWriter.println("SORRY USERNAME OR PASSWORD INCORRECT");
+
+            // the next statement sends the data of this servlet to index.html
             RequestDispatcher requestDispatcher = req.getRequestDispatcher("/index.html");
             requestDispatcher.include(req, resp);
         }
