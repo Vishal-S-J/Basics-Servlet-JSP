bfcbb9b610cbe5ca89d7b68b7518f7037d54c938
Vishal Jadhav <vsjjadhav1997@gmail.com>
2023-07-29 16:59:23 -0500

    code updated

--- ServletPart2Revised/src/main/java/org/example/App.java
+++ ServletPart2Revised/src/main/java/org/example/App.java
@@ -1,13 +1,22 @@
 package org.example;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        printWriter.println("HELLO THIS IS PRINTWRITER CLASS");
     }
 }
